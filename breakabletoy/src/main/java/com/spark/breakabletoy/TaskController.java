46628ee3a6376dc8a9a1710415c892eba696a09e
ramar0 <roberto.amaro@encora.com>
2025-07-31 15:53:58 -0600

    CORS Error detection

--- breakabletoy/src/main/java/com/spark/breakabletoy/TaskController.java
+++ breakabletoy/src/main/java/com/spark/breakabletoy/TaskController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/tasks")
+//CORS Error due to browser inconcistency, works on some tabs and not on others, being addressed
 @CrossOrigin(origins = "http://localhost:8080")
 public class TaskController {
     
